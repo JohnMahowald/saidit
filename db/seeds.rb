d342e337f9be488b4781787fa35ece07d663720a
CrookedKings <John.Mahowald@gmail.com>
2014-10-09 16:55:27 -0700

    update heroku depencencies and basic seed testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+gues = User.create!(username: "john@me.com", password: 'johnjohn');
